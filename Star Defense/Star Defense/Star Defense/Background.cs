4834ec1af2e609b63a31fda6aa78644991c16d92
rutkowsk14 <rutkowsk14@up.edu>
2014-01-28 21:44:42 -0800

    updated enemy to work properly with scrolling up

---
+++ Star Defense/Star Defense/Star Defense/Background.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Star_Defense
+{
+    class Background
+    {
+        // Textures to hold the two background images
+        Texture2D t2dBackground, t2dParallax;
+
+        int iViewportWidth = 720;
+        int iViewportHeight = 1920;
+
+        int iBackgroundWidth = 720;
+        int iBackgroundHeight = 1920;
+
+        int iParallaxWidth = 720;
+        int iParallaxHeight = 1680;
+
+        int iBackgroundOffset;
+        int iParallaxOffset;
+
+        public int BackgroundOffset
+        {
+            get { return iBackgroundOffset; }
+            set
+            {
+                iBackgroundOffset = value;
+                if (iBackgroundOffset < 0)
+                {
+                    iBackgroundOffset += iBackgroundHeight;
+                }
+                if (iBackgroundOffset > iBackgroundHeight)
+                {
+                    iBackgroundOffset -= iBackgroundHeight;
+                }
+            }
+        }
+
+        public int ParallaxOffset
+        {
+            get { return iParallaxOffset; }
+            set
+            {
+                iParallaxOffset = value;
+                if (iParallaxOffset < 0)
+                {
+                    iParallaxOffset += iParallaxHeight;
+                }
+                if (iParallaxOffset > iParallaxHeight)
+                {
+                    iParallaxOffset -= iParallaxHeight;
+                }
+            }
+        }
+
+        // Determines if we will draw the Parallax overlay.
+        bool drawParallax = true;
+
+        public bool DrawParallax
+        {
+            get { return drawParallax; }
+            set { drawParallax = value; }
+        }
+
+        // Constructor when passed a Content Manager and two strings
+        public Background(ContentManager content,
+                          string sBackground,
+                          string sParallax)
+        {
+
+            t2dBackground = content.Load<Texture2D>(sBackground);
+            iBackgroundWidth = t2dBackground.Width;
+            iBackgroundHeight = t2dBackground.Height;
+            t2dParallax = content.Load<Texture2D>(sParallax);
+            iParallaxWidth = t2dParallax.Width;
+            iParallaxHeight = t2dParallax.Height;
+        }
+
+        // Constructor when passed a content manager and a single string
+        public Background(ContentManager content, string sBackground)
+        {
+
+            t2dBackground = content.Load<Texture2D>(sBackground);
+            iBackgroundWidth = t2dBackground.Width;
+            iBackgroundHeight = t2dBackground.Height;
+            t2dParallax = t2dBackground;
+            iParallaxWidth = t2dParallax.Width;
+            iParallaxHeight = t2dParallax.Height;
+            drawParallax = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the background panel, offset by the player's location
+            spriteBatch.Draw(
+                t2dBackground,
+                new Rectangle(0,
+                              -1 * iBackgroundOffset, iBackgroundWidth,
+                              iViewportHeight),
+                Color.White);
+
+            // If the right edge of the background panel will end 
+            // within the bounds of the display, draw a second copy 
+            // of the background at that location.
+            if (iBackgroundOffset > iBackgroundHeight - iViewportHeight)
+            {
+                spriteBatch.Draw(
+                    t2dBackground,
+                    new Rectangle(
+                      0,
+                      (-1 * iBackgroundOffset) + iBackgroundHeight,
+                      iBackgroundWidth,
+                      iViewportHeight),
+                    Color.White);
+            }
+
+            if (drawParallax && false)
+            {
+                // Draw the parallax star field
+                spriteBatch.Draw(
+                    t2dParallax,
+                    new Rectangle(0,
+                                  -1 * iParallaxOffset, iParallaxWidth,
+                                  iViewportHeight),
+                    Color.SlateGray);
+                // if the player is past the point where the star 
+                // field will end on the active screen we need 
+                // to draw a second copy of it to cover the 
+                // remaining screen area.
+                if (iParallaxOffset > iParallaxHeight - iViewportHeight)
+                {
+                    spriteBatch.Draw(
+                        t2dParallax,
+                        new Rectangle(
+                          0,
+                          (-1 * iParallaxOffset) + iParallaxHeight,
+                          iParallaxWidth,
+                          iViewportHeight),
+                        Color.SlateGray);
+                }
+            }
+        }
+    }
+}
---
+++ Star Defense/Star Defense/Star Defense/Enemy.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Star_Defense
+{
+    class Enemy
+    {
+        AnimatedSprite asSprite;
+
+        static int iMapWidth = 1920;
+        static int iPlayAreaTop = 30;
+        static int iPlayAreaBottom = 630;
+        static Random rndGen = new Random();
+        int iX = 0;
+        int iY = -100;
+        int iBackgroundOffset = 0;
+        Vector2 v2motion = new Vector2(0f, 0f);
+        float fSpeed = 1f;
+        float fEnemyMoveCount = 0.0f;
+        float fEnemyDelay = 0.01f;
+        bool bActive = false;
+
+        public int X
+        {
+            get { return iX; }
+            set { iX = value; }
+        }
+
+        public int Y
+        {
+            get { return iY; }
+            set { iY = value; }
+        }
+
+        public bool IsActive
+        {
+            get { return bActive; }
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                int X = iX - iBackgroundOffset;
+                if (X > iMapWidth)
+                    X -= iMapWidth;
+                if (X < 0)
+                    X += iMapWidth;
+                return new Rectangle(X, iY, 32, 32);
+            }
+        }
+
+        public int Offset
+        {
+            get { return iBackgroundOffset; }
+            set { iBackgroundOffset = value; }
+        }
+
+        public float Speed
+        {
+            get { return fSpeed; }
+        }
+
+        public Vector2 Motion
+        {
+            get { return v2motion; }
+            set { v2motion = value; }
+        }
+
+        public Rectangle CollisionBox
+        {
+            get
+            {
+                int X = iX - iBackgroundOffset;
+                if (X > iMapWidth)
+                    X -= iMapWidth;
+                if (X < 0)
+                    X += iMapWidth;
+                return new Rectangle(X + 2, iY + 2, 28, 28);
+            }
+        }
+
+        public Enemy(Texture2D texture,
+              int X, int Y, int W, int H, int Frames)
+        {
+            asSprite = new AnimatedSprite(texture, X, Y, W, H, Frames);
+        }
+
+        public void Deactivate()
+        {
+            bActive = false;
+        }
+
+        private int GetDrawX()
+        {
+            int X = iX - iBackgroundOffset;
+            if (X > iMapWidth)
+
+                X -= iMapWidth;
+            if (X < 0)
+                X += iMapWidth;
+
+            return X;
+        }
+
+        public void RandomizeMovement()
+        {
+            v2motion.X = rndGen.Next(-50, 50);
+            v2motion.Y = rndGen.Next(-50, 50);
+            v2motion.Normalize();
+            fSpeed = (float)(rndGen.Next(3, 6));
+        }
+
+        public void Generate(int iLocation, int iShipX)
+        {
+            // Generate a random X location that is NOT 
+            // within 200 pixels of the player's ship.
+            do
+            {
+                iX = rndGen.Next(iMapWidth);
+                iBackgroundOffset = iLocation;
+            } while (Math.Abs(GetDrawX() - iShipX) < 200);
+
+            // Generate a random Y location between iPlayAreaTop 
+            // and iPlayAreaBottom (the area of our game screen)
+
+            iY = rndGen.Next(iPlayAreaTop, iPlayAreaBottom);
+            RandomizeMovement();
+            bActive = true;
+        }
+
+        public void Draw(SpriteBatch sb, int iLocation)
+        {
+            if (bActive)
+                asSprite.Draw(sb, GetDrawX(), iY, false);
+        }
+
+        public void Update(GameTime gametime, int iOffset)
+        {
+            iBackgroundOffset = iOffset;
+
+            fEnemyMoveCount += (float)gametime.ElapsedGameTime.TotalSeconds;
+            if (fEnemyMoveCount > fEnemyDelay)
+            {
+                iX += (int)((float)v2motion.X * fSpeed);
+                iY += (int)((float)v2motion.Y * fSpeed);
+
+                if (rndGen.Next(200) == 1)
+                {
+                    RandomizeMovement();
+                }
+
+                if (iY < iPlayAreaTop)
+                {
+                    iY = iPlayAreaTop;
+                    RandomizeMovement();
+                }
+
+                if (iY > iPlayAreaBottom)
+                {
+                    iY = iPlayAreaBottom;
+                    RandomizeMovement();
+                }
+
+                if (iX < 0)
+                    iX += iMapWidth;
+
+                if (iX > iMapWidth)
+                    iX -= iMapWidth;
+
+                fEnemyMoveCount = 0f;
+            }
+            asSprite.Update(gametime);
+        }
+    }
+}
